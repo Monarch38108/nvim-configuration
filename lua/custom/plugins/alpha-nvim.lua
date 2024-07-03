27f6c2721bf2bb1a679b4dd1a2dbc965195aac8c
monarch38108 <monarch38108@gmail.com>
2024-07-03 12:58:20 +0200

    Added Alpha-Nvim design

--- lua/custom/plugins/alpha-nvim.lua
+++ lua/custom/plugins/alpha-nvim.lua
@@ -1,10 +1,39 @@
+
 return {
-    'goolord/alpha-nvim',
-    dependencies = {
-        'nvim-tree/nvim-web-devicons',
-        'nvim-lua/plenary.nvim'
-    },
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.theta'.config)
-    end
-};
+  'goolord/alpha-nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'shellheim/ashaar.nvim',
+  },
+
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
+
+        -- Call ashaar
+    local ashaar = require 'ashaar'
+
+    -- Set header
+    dashboard.section.header.val = {
+      '███▄▄▄▄      ▄████████  ▄██████▄   ▄█    █▄   ▄█    ▄▄▄▄███▄▄▄▄   ',
+      '███▀▀▀██▄   ███    ███ ███    ███ ███    ███ ███  ▄██▀▀▀███▀▀▀██▄ ',
+      '███   ███   ███    █▀  ███    ███ ███    ███ ███▌ ███   ███   ███ ',
+      '███   ███  ▄███▄▄▄     ███    ███ ███    ███ ███▌ ███   ███   ███ ',
+      '███   ███ ▀▀███▀▀▀     ███    ███ ███    ███ ███▌ ███   ███   ███ ',
+      '███   ███   ███    █▄  ███    ███ ███    ███ ███  ███   ███   ███ ',
+      '███   ███   ███    ███ ███    ███ ███    ███ ███  ███   ███   ███ ',
+      ' ▀█   █▀    ██████████  ▀██████▀   ▀██████▀  █▀    ▀█   ███   █▀  ',
+    }
+    dashboard.section.buttons.val = {
+      dashboard.button('n', ' > New File', '<cmd>ene<CR>'),
+      dashboard.button('f', '󰱼 > Find File', '<cmd>Telescope find_files<CR>'),
+      dashboard.button('r', ' > Find Recent Files', '<cmd>Telescope oldfiles<CR>'),
+      dashboard.button('c', ' > Configuration', '<cmd>edit ~/.config/nvim/init.lua<CR>'),
+      dashboard.button('q', ' > Quit NVIM', '<cmd>qa<CR>'),
+    }
+        -- Give the returned couplet to alpha's footer
+    dashboard.section.footer.val = ashaar()
+
+    alpha.setup(dashboard.opts)
+  end,
+}
